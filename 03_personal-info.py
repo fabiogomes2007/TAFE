54536db5958beadc8317b027e9c12891de81491b
fabiogomes2007 <44798942+fabiogomes2007@users.noreply.github.com>
2018-11-06 20:46:30 +1100

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,8 @@
+first=input("What is your first name ")
+last=input("What is your last name ")
+location=input("What is your location ")
+age=input("How old are you ")
+sp= " "
+
+
+print("Hello " + first + sp + last + ". Your location is" + location + " and you age is " + age + ".")
